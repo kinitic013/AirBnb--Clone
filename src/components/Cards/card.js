fa42166c5b2511b044910b989f8e44709cfe0e49
kinitic013 <abhishektamrakar013@gmail.com>
2023-10-30 16:48:46 +0530

    Added image swiper

--- src/components/Cards/card.js
+++ src/components/Cards/card.js
@@ -1,12 +1,13 @@
 import "./styles.css";
 import StarIcon from "@mui/icons-material/Star";
-import React, { useRef, useState } from 'react';
-  
+import React from "react";
+import Carousel from "./swip";
 function Card(props) {
   return (
     <div className="card">
-      <div classNmae="card-img-container">
-        <img src={props.imgSrc[0]} alt="card-img" className="card-img"></img>
+      <div classNmae="card-img-container containerStyles">
+        <Carousel data={props.imgSrc}/>
+        {/* <img src={props.imgSrc[0]} alt="card-img" className="card-img"></img> */}
       </div>
       <div className="card-title">
         <div className="card-title-desc">{props.desc}</div>
@@ -23,7 +24,3 @@ function Card(props) {
   );
 }
 export default Card;
-
-
-
-
